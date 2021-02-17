0631e348b3baefcb551dae45ac52320ef1e6a909
aalbaali <amro.al-baali@mail.mcgill.ca>
2021-02-16 23:04:20 -0500

    Removed the 'simulator' code from the main cpp file

--- tutorial_slam2d.cpp
+++ tutorial_slam2d.cpp
@@ -48,12 +48,8 @@ using namespace g2o::tutorial;
 
 int main()
 {
-  // TODO simulate different sensor offset
-  // simulate a robot observing landmarks while travelling on a grid
-  SE2 sensorOffsetTransf(0.2, 0.1, -0.1);
+  // Number of nodes (or poses)
   int numNodes = 3;
-  Simulator simulator;
-  simulator.simulate(numNodes, sensorOffsetTransf);
 
   /*********************************************************************************
    * creating the optimization problem
@@ -71,24 +67,14 @@ int main()
 
   optimizer.setAlgorithm(solver);
 
-  // add the parameter representing the sensor offset
-  ParameterSE2Offset* sensorOffset = new ParameterSE2Offset;
-  sensorOffset->setOffset(sensorOffsetTransf);
-  sensorOffset->setId(0);
-  optimizer.addParameter(sensorOffset);
-
   // adding the odometry to the optimizer
   // first adding all the vertices
   cerr << "Optimization: Adding robot poses ... ";
-  for (size_t i = 0; i < simulator.poses().size(); ++i) {
-    // const Simulator::GridPose& p = simulator.poses()[i];
-    // const SE2& t = p.simulatorPose;
-    // AA: Set my own estimate    
+  for (size_t i = 0; i < numNodes; ++i) {
     VertexSE2* robot =  new VertexSE2;
     robot->setId( i);
     // AA : set my own estimate
-    robot->setEstimate( SE2( 1., 2., 0.01));
-    // robot->setEstimate(t);
+    robot->setEstimate( SE2( 0., 0., 0.0));
     optimizer.addVertex(robot);
   }
   cerr << "done." << endl;
@@ -96,50 +82,16 @@ int main()
   // second add the odometry constraints
   cerr << "Optimization: Adding odometry measurements ... ";
   for (size_t i = 0; i < numNodes - 1; ++i) {
-    const Simulator::GridEdge& simEdge = simulator.odometry()[i];
-
-    // EdgeSE2* odometry = new EdgeSE2;
-    // odometry->vertices()[0] = optimizer.vertex(simEdge.from);
-    // odometry->vertices()[1] = optimizer.vertex(simEdge.to);
-    // odometry->setMeasurement(simEdge.simulatorTransf);
-    // odometry->setInformation(simEdge.information);
-    // optimizer.addEdge(odometry);
-
     EdgeSE2* odometry = new EdgeSE2;
     odometry->vertices()[0] = optimizer.vertex( i);
     odometry->vertices()[1] = optimizer.vertex( i + 1);
     // Takes SE(2) measurement
-    odometry->setMeasurement( SE2( 1., 0., 0.));
+    odometry->setMeasurement( SE2( 1., 0., 0.0));
     // Informaitonmatrix on the measurement
     odometry->setInformation( Eigen::Matrix3d::Identity());
     optimizer.addEdge(odometry);
   }
   cerr << "done." << endl;
-
-  // // add the landmark observations
-  // cerr << "Optimization: add landmark vertices ... ";
-  // for (size_t i = 0; i < simulator.landmarks().size(); ++i) {
-  //   const Simulator::Landmark& l = simulator.landmarks()[i];
-  //   VertexPointXY* landmark = new VertexPointXY;
-  //   landmark->setId(l.id);
-  //   landmark->setEstimate(l.simulatedPose);
-  //   optimizer.addVertex(landmark);
-  // }
-  // cerr << "done." << endl;
-
-  // cerr << "Optimization: add landmark observations ... ";
-  // for (size_t i = 0; i < simulator.landmarkObservations().size(); ++i) {
-  //   const Simulator::LandmarkEdge& simEdge = simulator.landmarkObservations()[i];
-  //   EdgeSE2PointXY* landmarkObservation =  new EdgeSE2PointXY;
-  //   landmarkObservation->vertices()[0] = optimizer.vertex(simEdge.from);
-  //   landmarkObservation->vertices()[1] = optimizer.vertex(simEdge.to);
-  //   landmarkObservation->setMeasurement(simEdge.simulatorMeas);
-  //   landmarkObservation->setInformation(simEdge.information);
-  //   landmarkObservation->setParameterId(0, sensorOffset->id());
-  //   optimizer.addEdge(landmarkObservation);
-  // }
-  // cerr << "done." << endl;
-
 
   /*********************************************************************************
    * optimization
